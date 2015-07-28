9c76c51c9f1258b8d73c418d3146b9b2a9c37986
cuseros <carlos_useros@hotmail.com>
2015-07-29 00:55:52 +0200

    Autoload con "findById" en lugar de "find"

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz de Carlos' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId, es decir, ejecuta load si existe quizId
+
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
