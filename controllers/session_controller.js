93a57cf531cd0b8c69f2faf1e17913088d9fd4d9
cuseros <carlos_useros@hotmail.com>
2015-08-07 01:28:48 +0200

    Autorización
    Módulo 9.3

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,6 +1,13 @@
 
 // Middleware: Se requiere hacer login.
 //
+exports.loginRequired = function(req, res, next){
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+};
 
 
 // Formulario para hacer login
